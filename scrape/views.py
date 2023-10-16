5314d3a1cd5c6d04a647846f8c1c4c20a10dec82
traxx900 <rosenmilev@gmail.com>
2023-10-16 23:32:05 +0300

    Add filter

--- scrape/views.py
+++ scrape/views.py
@@ -4,21 +4,33 @@ from bs4 import BeautifulSoup
 
 
 def index(request):
-	output = []
+	output_all_jobs = []
+	output_python_jobs = []
 	URL = "https://realpython.github.io/fake-jobs/"
 	page = requests.get(URL)
 	soup = BeautifulSoup(page.content, "html.parser")
 	results = soup.find(id="ResultsContainer")
 	job_elements = results.find_all("div", class_="card-content")
-	results = results.prettify()
-	for job_element in job_elements:
-		title = job_element.find('h2', class_='title')
-		company = job_element.find('h3', class_='company')
-		location = job_element.find('p', class_='location')
-		output.append([title.text, company.text, location.text])
+
+	def html_to_text(html_result, output):
+		for el in html_result:
+			title = el.find('h2', class_='title')
+			company = el.find('h3', class_='company')
+			location = el.find('p', class_='location')
+			if title:
+				output.append([title.text, company.text, location.text])
+		return output
+
+	python_jobs = results.find_all(
+		"h2", string=lambda text: "python" in text.lower()
+	)
+
+	html_to_text(job_elements, output_all_jobs)
+	html_to_text(python_jobs, output_python_jobs)
 
 	context = {
 
-		'result': output
+		'all_jobs': output_all_jobs,
+		'python_jobs': output_python_jobs
 	}
 	return render(request, 'index.html', context)
